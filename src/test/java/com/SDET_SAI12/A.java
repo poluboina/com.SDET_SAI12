8b7888fe52b56934e415a26077aad39ad913bd73
saikr <saikr@LAPTOP-C09100BS>
2022-07-29 13:29:30 +0530

    commiting to master

---
+++ src/test/java/com/SDET_SAI12/A.java
@@ -0,0 +1,10 @@
+package com.SDET_SAI12;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("A....");
+
+	}
+
+}
---
+++ src/test/java/com/SDET_SAI12/C.java
@@ -0,0 +1,9 @@
+package com.SDET_SAI12;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("C....");
+	}
+
+}
